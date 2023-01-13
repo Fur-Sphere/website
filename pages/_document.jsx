06e6460f3a17f224d7c6ae5fea8925de367eee6a
Ravindra Nag <ravindranag52@gmail.com>
2023-01-14 03:06:02 +0530

    feat: seo

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,20 @@ export default function Document() {
     <Html lang="en">
       <Head>
 				<base target='_blank' />
+				<link 
+					rel='icon' 
+					href='/favicon-on-light.ico'
+					media='(prefers-color-scheme: light)' 
+				/>
+				<link 
+					rel='icon' 
+					href='/favicon-on-dark.ico'
+					media='(prefers-color-scheme: dark)' 
+				/>
+				<meta 
+					name='description' 
+					content='furSphere, being the ultimate fur-care for your tiny pets empowers you with all the emergency help by getting you the specific details of the Pet centers, Training centers, Pet NGOs, Vet Hospitals, and accessories.'
+				/>
 			</Head>
       <body>
         <Main />
