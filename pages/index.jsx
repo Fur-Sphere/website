9ce5a6426ce120f19c79debead9443f856bad7f3
Ravindra Nag <ravindranag52@gmail.com>
2023-01-13 23:24:36 +0530

    chore: apt extension

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,13 @@ import DefaultLayout from "../layout/DefaultLayout"
 import Hero from "../components/home/Hero"
 import Services from "../components/home/Services"
 import Prompt from "../components/home/Prompt"
+import About from "../components/home/About"
 
 const Home = () => {
 	return (
 		<Stack>
 			<Hero />
+			<About />
 			<Services />
 			<Prompt />
 		</Stack>
